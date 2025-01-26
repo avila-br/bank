2f60d3d9acce622c9e4c22e6caec0635129ed8fe
avila-br <avila.developer@proton.me>
2025-01-25 21:34:13 -0300

    feat: implement AccountType.from(String)

--- src/main/java/br/com/compass/bank/model/AccountType.java
+++ src/main/java/br/com/compass/bank/model/AccountType.java
@@ -1,10 +1,33 @@
 package br.com.compass.bank.model;
 
+import java.util.Optional;
+
 /**
  * Enum representing the type of account.
  */
 public enum AccountType {
     CHECKING, // Representing a checking account
     SAVINGS,  // Representing a savings account
-    BUSINESS  // Representing a business account
+    BUSINESS;  // Representing a business account
+
+    /**
+     * Returns an AccountType based on the given string.
+     *
+     * @param type String representing the account type (case-insensitive).
+     * @return AccountType corresponding to the string.
+     * @throws IllegalArgumentException if the string does not match any account type.
+     */
+    public static Optional<AccountType> from(String type) {
+        if (type == null) {
+            return Optional.empty();
+        }
+
+        // Convert the string to lower case and check for the corresponding AccountType
+        return switch (type.trim().toLowerCase()) {
+            case "checking", "corrente" -> Optional.of(CHECKING);
+            case "savings", "poupança" -> Optional.of(SAVINGS);
+            case "business", "jurídica" -> Optional.of(BUSINESS);
+            default -> Optional.empty();
+        };
+    }
 }
