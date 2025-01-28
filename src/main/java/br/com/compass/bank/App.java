c56977feab348688169dcf996ad305ff50cd3411
avila-br <avila.developer@proton.me>
2025-01-28 12:56:41 -0300

    fix: remove useless method

--- src/main/java/br/com/compass/bank/App.java
+++ src/main/java/br/com/compass/bank/App.java
@@ -38,56 +38,9 @@ public class App {
                 System.exit(0);
                 break;
             default:
-                // TODO
-                // System.out.println("Invalid option! Please try again.");
-                break;
-        }
-    }
-
-    public static void bankMenu(Scanner scanner) {
-        boolean running = true;
-
-        while (running) {
-            System.out.println("========= Bank Menu =========");
-            System.out.println("|| 1. Deposit              ||");
-            System.out.println("|| 2. Withdraw             ||");
-            System.out.println("|| 3. Check Balance        ||");
-            System.out.println("|| 4. Transfer             ||");
-            System.out.println("|| 5. Bank Statement       ||");
-            System.out.println("|| 0. Exit                 ||");
-            System.out.println("=============================");
-            System.out.print("Choose an option: ");
-
-            int option = scanner.nextInt();
-
-            switch (option) {
-                case 1:
-                    // ToDo...
-                    System.out.println("Deposit.");
-                    break;
-                case 2:
-                    // ToDo...
-                    System.out.println("Withdraw.");
-                    break;
-                case 3:
-                    // ToDo...
-                    System.out.println("Check Balance.");
-                    break;
-                case 4:
-                    // ToDo...
-                    System.out.println("Transfer.");
-                    break;
-                case 5:
-                    // ToDo...
-                    System.out.println("Bank Statement.");
-                    break;
-                case 0:
-                    // ToDo...
-                    System.out.println("Exiting...");
-                    running = false;
-                default:
-                    System.out.println("Invalid option! Please try again.");
-            }
+                 System.out.println("Invalid option! Please try again.");
+                 menu();
+                 break;
         }
     }
 
